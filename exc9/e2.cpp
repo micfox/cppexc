4191bae180c75b16854386bf184ff6e3217e8a08
Wu <jiatengwu@gmail.com>
2015-10-10 19:54:56 +0800

    add operator

--- exc9/e2.cpp
+++ exc9/e2.cpp
@@ -2,8 +2,18 @@
 
 int main()
 {
-	Name_pair np;
-	np.read_name();
-	np.read_age();
-	cout << np;
+	Name_pair np1{
+		vector<string>{"aa","bb","cc"},
+		vector<double>{50,60,70}
+	};
+	Name_pair np2{
+		vector<string>{"aa","bb"},
+		vector<double>{55,66}
+	};
+	//np.read_name();
+	//np.read_age();
+	//cout << np;
+
+	if(np1 == np2) cout << "name pairs equal";
+	if(np1 != np2) cout << "name pairs not equal";
 }
--- exc9/name_pair.cpp
+++ exc9/name_pair.cpp
@@ -1,5 +1,14 @@
 #include "name_pair.h"
 
+Name_pair::Name_pair(vector<string> nn, vector<double> aa)
+	:name{nn},age{aa}
+{
+	if(name.size() != age.size()) throw Invalid{};
+}
+
+Name_pair::Name_pair()
+{
+}
 void Name_pair::read_name()
 {
 	string s;
@@ -23,11 +32,11 @@ void Name_pair::read_age()
 	
 }
 
-vector<string> Name_pair::get_name()
+vector<string>& Name_pair::get_name()
 {
 	return name;
 }
-vector<double> Name_pair::get_age()
+vector<double>& Name_pair::get_age()
 {
 	return age;
 }
@@ -39,7 +48,7 @@ void Name_pair::print() const
 			<< age[i] << '\n';
 }
 
-ostream& operator<<(ostream& os, Name_pair np)
+ostream& operator<<(ostream& os, Name_pair& np)
 {
 	vector<string> name = np.get_name();
 	vector<double> age = np.get_age();
@@ -48,3 +57,20 @@ ostream& operator<<(ostream& os, Name_pair np)
 		s+= name[i] + ' ' + to_string(age[i]) + '\n';
 	return os << s;
 }
+
+bool operator==(Name_pair& np1, Name_pair& np2)
+{
+	vector<string> name1 = np1.get_name();
+	vector<string> name2 = np2.get_name();
+	if( name1.size() != name2.size() ) return false;
+	for(int i=0;i<name1.size();++i)
+		if( name1[i] != name2[i] ) return false;
+	return true;
+}
+
+bool operator!=(Name_pair& np1, Name_pair& np2)
+{
+	if( np1 == np2 ) return false;
+	return true;
+}
+
