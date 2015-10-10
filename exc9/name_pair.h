3557a508b196f866c75a2bdf75d6580e7d0c8bdd
Wu <jiatengwu@gmail.com>
2015-10-10 19:56:08 +0800

    ed file

--- exc9/name_pair.h
+++ exc9/name_pair.h
@@ -6,12 +6,18 @@ class Name_pair{
 		vector<double> age;
 
 	public:
+		class Invalid{};
+		Name_pair(vector<string> name, vector<double> age);
+		Name_pair();
 		void read_name();
 		void read_age();
 		void print() const; 
 		void sort();
-		vector<string> get_name();
-		vector<double> get_age();
+		vector<string>& get_name();
+		vector<double>& get_age();
 };
 
 ostream& operator<<(ostream& os, Name_pair np);
+bool operator==(Name_pair&, Name_pair&);
+bool operator!=(Name_pair&, Name_pair&);
+bool operator==(Name_pair&, Name_pair&);
