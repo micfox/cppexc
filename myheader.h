d3ea67bd6367b81c1390ec2047e3ea7df6120d3a
Jiateng <327540310@qq.com>
2017-02-10 08:28:19 +0800

    update myheader

---
+++ myheader.h
@@ -0,0 +1,16 @@
+#include "std_lib_facilities.h"
+#define Debug_print(a) std::cout << "Debug >> " << #a << ": " << (a) << std::endl
+
+//vector print
+template<typename T>
+ostream& operator<<(ostream& os, vector<T>& v)
+{
+    os << "{ ";
+    for(T t : v) {
+        os << t;
+        os << ' ';
+    }
+    os << '}';
+    return os;
+}
+
