2ce8f9e96c30decb0123e47398d1be6140674eab
Jiateng <327540310@qq.com>
2017-02-10 08:28:04 +0800

    update myheader

--- 1901.cpp
+++ 1901.cpp
@@ -1,16 +1,5 @@
-#include "std_lib_facilities.h"
-
-template<typename T>
-ostream& operator<<(ostream& os, vector<T>& v)
-{
-    os << "{ ";
-    for(T t : v) {
-        os << t;
-        os << ' ';
-    }
-    os << '}';
-    return os;
-}
+//#include "std_lib_facilities.h"
+#include "myheader.h"
 
 template<typename T>
 void f(T& a, const T& b)
@@ -20,11 +9,24 @@ void f(T& a, const T& b)
 	}
 }
 
+template<typename T>
+vector<T> vtmul(vector<T>& v1, vector<T>& v2)
+{
+	vector<T> result;
+	for (int i = 0; i < v1.size() && i < v2.size(); ++i) {
+		Debug_print(v1[i] * v2[i]);
+		result.push_back(v1[i] * v2[i]);
+	}
+	return result;
+}
+
 int main()
 {
 	vector<int> vi1 {1,2,3,4,5};
 	vector<int> vi2 {2,3,4,995};
 	f(vi1, vi2);
-	cout << vi1;
+	//cout << vi1;
+	vector<int> r {vtmul(vi1, vi2)};
+	cout << r;
 }
 
